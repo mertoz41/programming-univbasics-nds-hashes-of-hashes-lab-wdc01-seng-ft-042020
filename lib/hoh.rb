e5a807a5a07dcc24cc9af6b522a7828e1e53517f
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-03-23 17:00:27 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -45,8 +45,7 @@ def naming_system
              :label => "Genus",
              :sub_category => {
                :label => "Species",
-               :sub_category => {
-                :sub_category => nil,
+               :sub_category => nil,
                }
              }
            }
